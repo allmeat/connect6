2f7d9e7f322c44907c2ef4108bc969a60ad098f8
completeresidue <hyoungseok.k@gmail.com>
2019-08-23 00:19:02 +0900

    [Python] create board server

---
+++ server.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from board import Board
+from bokeh.plotting import output_file, save
+from flask import Flask, render_template, request
+
+
+@dataclass
+class Stone:
+    x: int
+    y: int
+    stone: str
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+board_object = Board()
+output_file("templates/board.html")
+save(board_object.board)
+log = []
+
+
+@app.route("/")
+def index():
+    global log
+    return render_template("board.html")
+
+
+@app.route("/play", methods=["GET"])
+def play():
+    # TODO : input regex check
+    x = request.args.get("x")
+    y = request.args.get("y")
+    stone = request.args.get("stone")
+    log.append(Stone(x, y, stone))
+    board_object.put_stone(x, y, stone)
+    save(board_object.board)
+    return render_template("board.html")
+
+
+@app.route("/logging")
+def logging():
+    return "/".join([f"{i + 1},{item.x},{item.y},{item.stone}" for i, item in enumerate(log)])
+
+
+if __name__ == "__main__":
+    app.run(port=8080)
