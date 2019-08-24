c078af17ec20a3746ca9f4c08eec5b38e3503598
completeresidue <hyoungseok.k@gmail.com>
2019-08-25 00:02:02 +0900

    [Python] add input sanity check

--- server.py
+++ server.py
@@ -1,14 +1,7 @@
-from dataclasses import dataclass
-from board import Board
+import re
+from board import Board, Stone
 from bokeh.plotting import output_file, save
 from flask import Flask, render_template, request
-
-
-@dataclass
-class Stone:
-    x: int
-    y: int
-    stone: str
 
 
 app = Flask(__name__)
@@ -16,7 +9,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 board_object = Board()
 output_file("templates/board.html")
 save(board_object.board)
-log = []
+regex_digit = re.compile(r"\d+")
+regex_stone = re.compile(r"[bw]")
 
 
 @app.route("/")
@@ -26,24 +20,31 @@ def index():
 
 @app.route("/play", methods=["GET"])
 def play():
-    # TODO : input regex check
     x = request.args.get("x")
     y = request.args.get("y")
     stone = request.args.get("stone")
-    log.append(Stone(x, y, stone))
-    board_object.put_stone(x, y, stone)
-    save(board_object.board)
-    return render_template("board.html")
+
+    if regex_digit.match(x) is None:
+        return {"code": 400, "message": "illegal input in x"}, 400
+    elif regex_digit.match(y) is None:
+        return {"code": 400, "message": "illegal input in y"}, 400
+    elif regex_stone.match(stone) is None:
+        return {"code": 400, "message": "illegal input in stone"}, 400
+    else:
+        board_object.log.append(Stone(x, y, stone))
+        board_object.put_stone(x, y, stone)
+        save(board_object.board)
+        return render_template("board.html")
 
 
-@app.route("/logging")
-def logging():
-    return "/".join([f"{i + 1},{item.x},{item.y},{item.stone}" for i, item in enumerate(log)])
+@app.route("/history")
+def history():
+    return "/".join([f"{i + 1},{item.x},{item.y},{item.stone}" for i, item in enumerate(board_object.log)])
 
 
 @app.route("/reset")
 def reset():
-    log.clear()
+    board_object.log.clear()
     board_object.setup()
     save(board_object.board)
     return render_template("board.html")
