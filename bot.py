1980301219a3ab46f7dffd4f064e4bc8c03aca88
completeresidue <hyoungseok.k@gmail.com>
2019-08-24 23:25:31 +0900

    [Python] create random bot

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-# random_bot : random 착수하는 bot
+from random import randint
+from server import Stone
+
+
+def random_bot(stone: str):
+    x = randint(1, 19)
+    y = randint(1, 19)
+    return Stone(x, y, stone)
+
+
+if __name__ == "__main__":
+    print(random_bot("w"))
