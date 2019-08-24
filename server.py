566715c7f937a4ea940367bf9679a57216900658
completeresidue <hyoungseok.k@gmail.com>
2019-08-25 01:24:32 +0900

    [Python] fix variable name

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import re
 import json
-from bokeh.plotting import output_file, save
 from flask import Flask, render_template, request
 from board import Board, Stone
 
@@ -8,10 +7,9 @@ from board import Board, Stone
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 board = Board()
-output_file("templates/board.html")
-save(board.figure)
+board.save_figure()
 regex_digit = re.compile(r"\d+")
-regex_stone = re.compile(r"[bw]")
+regex_color = re.compile(r"[bw]")
 
 
 @app.route("/")
@@ -23,24 +21,25 @@ def index():
 def play():
     x = request.args.get("x")
     y = request.args.get("y")
-    stone = request.args.get("stone")
+    color = request.args.get("color")
 
     if regex_digit.match(x) is None:
         return {"code": 400, "message": "illegal input in x"}, 400
     elif regex_digit.match(y) is None:
         return {"code": 400, "message": "illegal input in y"}, 400
-    elif regex_stone.match(stone) is None:
+    elif regex_color.match(color) is None:
         return {"code": 400, "message": "illegal input in stone"}, 400
     else:
-        board.log.append(Stone(x, y, stone))
-        board.put_stone(x, y, stone)
-        save(board.figure)
+        # TODO : set referee here
+        board.log.append(Stone(x, y, color))
+        board.put_stone(Stone(x, y, color))
+        board.save_figure()
         return render_template("board.html")
 
 
 @app.route("/history")
 def history():
-    board_log = [f"{item.x},{item.y},{item.stone}" for item in board.log]
+    board_log = [f"{item.x},{item.y},{item.color}" for item in board.log]
     return json.dumps({"code": 200, "data": board_log})
 
 
@@ -48,7 +47,7 @@ def history():
 def reset():
     board.log.clear()
     board.setup()
-    save(board.figure)
+    board.save_figure()
     return render_template("board.html")
 
 
