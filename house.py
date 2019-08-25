6a5a5d4a9ebb7f153ff250fd20fb7bbec4f31d09
completeresidue <hyoungseok.k@gmail.com>
2019-08-25 20:29:56 +0900

    [Python] make house class

--- house.py
+++ house.py
@@ -1,45 +1,56 @@
+from random import random
 from board import Board
 from referee import Referee
 from player import Player
 from bot import Bot
 
-board = Board()
-referee = Referee()
 
-player_first = False
-if player_first:
-    player = Player("b")
-    bot = Bot("w")
-else:
-    player = Player("w")
-    bot = Bot("b")
+class House:
 
+    def __init__(self, player_first: bool):
+        self.board = Board()
+        self.referee = Referee()
+        self.player_first = player_first
 
-def play(order: str, color: str):
-    while referee.turn_check(board.log) == color:
-        if order == "player":
-            stone = player.manual_input()
+        if self.player_first:
+            self.player = Player("b")
+            self.bot = Bot("w")
         else:
-            stone = bot.random_bot()
+            self.player = Player("w")
+            self.bot = Bot("b")
 
-        if referee.valid_check(stone, board.log):
-            board.put_stone(stone)
-            print(f"{order} stone: {stone.x},{stone.y},{color}")
-        else:
-            print("invalid stone input")
+    def play(self, order: str, color: str):
+        while self.referee.turn_check(self.board.log) == color:
+            if order == "player":
+                stone = self.player.manual_input()
+            else:
+                stone = self.bot.random_bot()
+
+            if self.referee.valid_check(stone, self.board.log):
+                self.board.put_stone(stone)
+                print(f"{order} stone: {stone.x},{stone.y},{color}")
+            else:
+                print("invalid stone input")
+
+    def game(self):
+        while self.referee.end_check(self.board.log) == "keep play":
+            if self.player_first:
+                self.play("player", "b")
+                if self.referee.end_check(self.board.log) != "keep play":
+                    break
+                self.play("bot", "w")
+            else:
+                self.play("bot", "b")
+                if self.referee.end_check(self.board.log) != "keep play":
+                    break
+                self.play("player", "w")
+
+        print(self.referee.end_check(self.board.log))
+        self.board.render_figure()
 
 
-while referee.end_check(board.log) == "keep play":
-    if player_first:
-        play("player", "b")
-        if referee.end_check(board.log) != "keep play":
-            break
-        play("bot", "w")
-    else:
-        play("bot", "b")
-        if referee.end_check(board.log) != "keep play":
-            break
-        play("player", "w")
-
-print(referee.end_check(board.log))
-board.render_figure()
+if __name__ == "__main__":
+    coin_toss = True if random() > 0.5 else False
+    print("player first: ", coin_toss)
+    house = House(coin_toss)
+    house.game()
