f54ab1925acd44401ab156a275ed4932fdb75d4e
서태희 <tei@seotaehuiui-MacBook-Pro.local>
2019-08-26 20:32:35 +0900

    aa

--- board.py
+++ board.py
@@ -14,7 +14,7 @@ def np2board(x):
 def drawboard():
 
     alphabet = "a b c d e f g h i j k l m n o p q r s t u".split(" ")
-    bar =
+
     rowIndex = " ".join(list(map(lambda y : y, alphabet)))
     a1 = list(map(lambda y: " ".join([np2board(x) for i, x in enumerate(y)]), aa))
     a2 = rowIndex + "\n" + "\n".join(a1)
