79e32510584c1b444706d9362f49779ebcf1d328
hyoungseok <hyoungseok.k@gmail.com>
2019-12-01 19:37:01 +0900

    [Python] #24 make direction constants

--- playground/util.py
+++ playground/util.py
@@ -6,3 +6,30 @@ def turn_check(log: List[Stone], each_move: int, first_move: int) -> str:
     if (len(log) - first_move) % (2 * each_move) in list(range(0, each_move)):
         return "w"
     return "b"
+
+
+class Direction:
+
+    HORIZONTAL = "h"
+    VERTICAL = "v"
+    NEGATIVE_DIAGONAL = "nd"
+    POSITIVE_DIAGONAL = "pd"
+
+    UP = "u"
+    DOWN = "d"
+    LEFT = "l"
+    RIGHT = "r"
+    UP_LEFT = "ul"
+    UP_RIGHT = "ur"
+    DOWN_LEFT = "dl"
+    DOWN_RIGHT = "dr"
+    ALL_DIRECTIONS = [
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        UP_LEFT,
+        UP_RIGHT,
+        DOWN_LEFT,
+        DOWN_RIGHT,
+    ]
