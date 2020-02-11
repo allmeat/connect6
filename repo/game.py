6df1f01799ce5e4f5adf594b6cb3d89151aa5402
jungwon.choi <jungwon.choi@linecorp.com>
2020-02-11 23:51:00 +0900

    [Python] #36 update schema

--- repo/game.py
+++ repo/game.py
@@ -1,7 +1,7 @@
 import datetime
 from dataclasses import dataclass
 from typing import List
-
+from pymysql import connections
 from board import Stone, BoardConfig
 
 
@@ -24,7 +24,10 @@ class GameLog:
     y_axis: int
 
 
-def convert_to_game(winner: str, board_config: BoardConfig, black_player: str, white_player: str,
+def convert_to_game(winner: str,
+                    board_config: BoardConfig,
+                    black_player: str,
+                    white_player: str,
                     logs: List[Stone]) -> Game:
     return Game(winner=winner,
                 total_size=len(logs),
@@ -34,5 +37,38 @@ def convert_to_game(winner: str, board_config: BoardConfig, black_player: str, w
                 created_time=datetime.datetime.now)
 
 
-def convert_to_game_log(game_id: int, index: int, stone: Stone) -> GameLog:
-    return GameLog(game_id=game_id, idx=index, stone_color=stone.color, x_axis=int(stone.x), y_axis=int(stone.y))
+def convert_to_game_log(game_id: int,
+                        index: int,
+                        stone: Stone) -> GameLog:
+    return GameLog(game_id=game_id,
+                   idx=index,
+                   stone_color=stone.color,
+                   x_axis=int(stone.x),
+                   y_axis=int(stone.y))
+
+
+def save_game(db: connections.Connection,
+              winner: str,
+              board_config: BoardConfig,
+              black_player: str,
+              white_player: str,
+              logs: List[Stone]):
+    game = convert_to_game(winner=winner, board_config=board_config, black_player=black_player,
+                           white_player=white_player, logs=logs)
+    cursor = db.cursor()
+    query = ""  # need a function to create query
+    cursor.execute(query)
+    db.commit()
+    db.close()
+
+
+def save_game_logs(db: connections.Connection,
+                   game_id: int,
+                   logs: List[Stone]):
+    for i, stone in enumerate(logs):
+        game_log = convert_to_game_log(game_id=game_id, index=i, stone=stone)
+        cursor = db.cursor()
+        query = ""  # need a function to create query
+        cursor.execute(query)
+        db.commit()
+        db.close()
