c449fa94a9938a32fb4978bb0b3ea4fdda70a87d
hyoungseok <hyoungseok.k@gmail.com>
2020-02-17 20:36:05 +0900

    [Python] #38 make class for DB connection

--- playground/connect_db.py
+++ playground/connect_db.py
@@ -1,32 +1,54 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 """[reference] https://docs.sqlalchemy.org/en/13/orm/tutorial.html"""
 
-engine = create_engine("mysql+pymysql://root:allmeat@35.233.179.226:3306/playground_log", echo=True)
 
-base = declarative_base()
+class DBConfig:
 
-session = sessionmaker(bind=engine)()
+    def __init__(self):
+        super(DBConfig, self).__init__()
+        self.session_maker = sessionmaker()
+
+    def setup_db_connection(self, uri, echo=False) -> Session:
+        engine = create_engine(uri, echo=echo)
+        self.session_maker.configure(bind=engine)
+        session = self.session_maker()
+        return session
+
+    @staticmethod
+    def insert(session, item):
+        session.add(item)
+        session.commit()
+
+    @staticmethod
+    def insert_list(session, item_list):
+        session.add_all(item_list)
+        session.commit()
 
 
-# TODO: need dataclass to DB table class converter
-class TESTTABLE(base):
-    __tablename__ = "TESTTABLE"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    age = Column(Integer)
+if __name__ == "__main__":
+    base = declarative_base()
 
 
-user_list = [
-    TESTTABLE(name="glex", age=31),
-    TESTTABLE(name="hlex", age=32),
-    TESTTABLE(name="ilex", age=33),
-]
+    class TESTTABLE(base):
+        __tablename__ = "TESTTABLE"
 
-session.add_all(user_list)
+        id = Column(Integer, primary_key=True)
+        name = Column(String)
+        age = Column(Integer)
 
-session.commit()
+    user_list = [
+        TESTTABLE(name="mlex", age=31),
+        TESTTABLE(name="nlex", age=32),
+        TESTTABLE(name="olex", age=33),
+    ]
+
+    db = DBConfig()
+    sess = db.setup_db_connection("mysql+pymysql://root:allmeat@35.233.179.226:3306/playground_log")
+    db.insert(sess, user_list[0])
+    # db.insert_list(sess, user_list)
+
+    print("insert done")
