bf80d6c9e05ad0ae11598e528792d6d5cf286730
chzlithoo <chzlithoo@gmail.com>
2020-02-24 17:19:47 +0900

    [Python] #36 add repo

---
+++ repo/game.py
@@ -0,0 +1,98 @@
+import datetime
+import json
+from typing import List
+
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import Session
+
+from board import Stone, BoardConfig
+
+
+class Game:
+    __tablename__ = "game"
+
+    id: Column(Integer, primary_key=True)
+    winner = Column(String)
+    total_size = Column(Integer)
+    board_config = Column(String)
+    black_player = Column(String)
+    white_player = Column(String)
+    created_time = Column(DateTime)
+
+    def __init__(self,
+                 winner: str,
+                 board_config: BoardConfig,
+                 black_player: str,
+                 white_player: str,
+                 logs: List[Stone],
+                 ):
+        self.winner = winner,
+        self.total_size = len(logs),
+        self.board_config = json.dumps(board_config.__dict__),
+        self.black_player = black_player,
+        self.white_player = white_player,
+        self.created_time = datetime.datetime.now
+
+
+class GameLog:
+    __tablename__ = "game_log"
+
+    id = Column(Integer, primary_key=True)
+    game_id = Column(Integer, ForeignKey("game.id"))  # id from GameRecord
+    idx = Column(Integer)
+    stone_color = Column(String)
+    x_axis = Column(Integer)
+    y_axis = Column(Integer)
+
+    def __init__(self,
+                 game_id: int,
+                 index: int,
+                 stone: Stone,
+                 ):
+        self.game_id = game_id,
+        self.idx = index,
+        self.stone_color = stone.color,
+        self.x_axis = int(stone.x),
+        self.y_axis = int(stone.y)
+
+
+def save_game_and_return_id(session: Session,
+                            winner: str,
+                            board_config: BoardConfig,
+                            black_player: str,
+                            white_player: str,
+                            logs: List[Stone],
+                            ) -> int:
+    game = Game(
+        winner=winner,
+        board_config=board_config,
+        black_player=black_player,
+        white_player=white_player,
+        logs=logs,
+    )
+    session.add(game)
+    session.flush()
+    game_id = game.id
+    session.commit()
+    return game_id
+
+
+def save_game_logs(session: Session,
+                   game_id: int,
+                   logs: List[Stone],
+                   ):
+    for i, stone in enumerate(logs):
+        game_log = GameLog(game_id=game_id, index=i, stone=stone)
+        session.add(game_log)
+        session.commit()
+
+
+def save_game_result(session: Session,
+                     winner: str,
+                     board_config: BoardConfig,
+                     black_player: str,
+                     white_player: str,
+                     logs: List[Stone],
+                     ):
+    game_id = save_game_and_return_id(session, winner, board_config, black_player, white_player, logs)
+    save_game_logs(session, game_id, logs)
---
+++ repo/game_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+from connect_db import DBConfig
+from game import Game
+from playground.board import BoardConfig
+from playground.house import Stone
+
+
+class GameTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(GameTest, self).__init__(*args, **kwargs)
+        self.engine = "mysql+pymysql://root:allmeat@35.233.179.226:3306/playground_log"
+
+    def test_save_game_logs(self):
+        db = DBConfig()
+        sess = db.setup_db_connection(self.engine)
+
+        winner = "monkey"
+        board_config = BoardConfig(19, 19, 6, 2, 1)
+        black_player = "dog"
+        white_player = "monkey"
+        logs = [Stone("3", "2", "b"),
+                Stone("2", "1", "w"),
+                Stone("2", "2", "w")]
+        game = Game(
+            winner=winner,
+            board_config=board_config,
+            black_player=black_player,
+            white_player=white_player,
+            logs=logs,
+        )
+        sess.add(game)
+        sess.flush()
+        gid = game.id
+        self.assertEqual(0, gid)
