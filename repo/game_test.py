de7633b8a1604ff14287e297465f3c9f8f99418e
chzlithoo <chzlithoo@gmail.com>
2020-03-02 20:55:17 +0900

    [Python] #36 reformat code

--- repo/game_test.py
+++ repo/game_test.py
@@ -7,6 +7,7 @@ from playground.house import Stone
 
 
 class GameTest(unittest.TestCase):
+
     def __init__(self, *args, **kwargs):
         super(GameTest, self).__init__(*args, **kwargs)
         self.engine = "mysql+pymysql://root:allmeat@35.233.179.226:3306/playground_log"
