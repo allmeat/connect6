bb8356638e7dab3004ced4c2d987f46cb665080b
alexrp <alexrp@company.ai>
2020-03-02 20:37:07 +0900

    [Python] #36 fix convention

--- repo/game_test.py
+++ repo/game_test.py
@@ -18,14 +18,19 @@ class GameTest(unittest.TestCase):
         board_config = BoardConfig(19, 19, 6, 2, 1)
         black_player = "dog"
         white_player = "monkey"
-        logs = [Stone("3", "2", "b"),
-                Stone("2", "1", "w"),
-                Stone("2", "2", "w")]
+        logs = [
+            Stone("3", "2", "b"),
+            Stone("2", "1", "w"),
+            Stone("2", "2", "w"),
+        ]
         gid = save_game_and_return_id(
-            session=self.session, winner=winner,
+            session=self.session,
+            winner=winner,
             board_config=board_config,
-            black_player=black_player, white_player=white_player,
-            logs=logs)
+            black_player=black_player,
+            white_player=white_player,
+            logs=logs,
+        )
         res = self.session.query(Game).filter(Game.id == gid).first()
         self.assertEqual(gid, res.id)
         self.assertEqual(winner, res.winner)
@@ -34,9 +39,11 @@ class GameTest(unittest.TestCase):
 
     def test_save_game_logs(self):
         game_id = 0
-        logs = [Stone("3", "2", "b"),
-                Stone("2", "1", "w"),
-                Stone("2", "2", "w")]
+        logs = [
+            Stone("3", "2", "b"),
+            Stone("2", "1", "w"),
+            Stone("2", "2", "w"),
+        ]
         save_game_logs(self.session, game_id, logs)
         res = self.session.query(Game).filter(GameLog.game_id == game_id).all()
         self.assertEqual(3, len(res))
@@ -47,7 +54,9 @@ class GameTest(unittest.TestCase):
         board_config = BoardConfig(19, 19, 6, 2, 1)
         black_player = "dog"
         white_player = "monkey"
-        logs = [Stone("3", "2", "b"),
-                Stone("2", "1", "w"),
-                Stone("2", "2", "w")]
+        logs = [
+            Stone("3", "2", "b"),
+            Stone("2", "1", "w"),
+            Stone("2", "2", "w"),
+        ]
         save_game_result(self.session, winner, board_config, black_player, white_player, logs)
